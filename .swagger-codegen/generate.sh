3a5905d4a777581d221937e66f77900468fc263d
Liam Morley <liam@carpeliam.com>
2025-01-20 09:23:14 -0500

    Update generated code, improve error message when swagger-codegen not available

--- .swagger-codegen/generate.sh
+++ .swagger-codegen/generate.sh
@@ -1,4 +1,12 @@
 ##!/bin/sh
-echo $0
-codegen_dir=$(dirname "$0")
-swagger-codegen generate --lang go --input-spec https://developer.shortcut.com/api/rest/v3/shortcut.swagger.json --output sc --config $codegen_dir/config.json
+if [ -x "$(command -v swagger-codegen)" ]; then
+    codegen_dir=$(dirname "$0")
+    swagger-codegen generate \
+        --lang go \
+        --input-spec https://developer.shortcut.com/api/rest/v3/shortcut.swagger.json \
+        --output generated \
+        --config $codegen_dir/config.json
+else
+    echo "Please install swagger-codegen: https://github.com/swagger-api/swagger-codegen | brew install swagger-codegen"
+    exit 1
+fi
